300cc168f55756de992cbadb9a9e08e372c9c0e0
Diego <diegolemosbraga@gmail.com>
2022-05-02 03:46:34 -0300

    Projeto Dados Covid19 Atualizados tempo Real

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import { Card, CardContent } from '../commons/styles/Card'
+import Button from '../commons/styles/Button'
+import Grid from '../commons/styles/Grid'
+import Typography from '../commons/styles/Typography'
+import Skeleton from '../commons/styles/Skeleton'
+import Select from '../commons/styles/Select'
+import MenuItem from '../commons/styles/MenuItem'
+
+export {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+  Skeleton,
+  Select,
+  MenuItem
+}
